09ba864382cba88cbef6db4fcf7996657d14441d
erratinsilentio <kacper.kondracki@wp.pl>
2022-12-30 11:29:14 +0100

    added materialui navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import { ResponsiveAppBar } from "./components/menu/Menu";
 
 function App() {
-
-    return (
-        <div className="App">
-        </div>
-    )
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
