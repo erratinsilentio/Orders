efb9e7173cfc6ff964746422c01c98c6ebe8fa97
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-05 00:41:26 +0100

    added styling to client details page

---
+++ client/src/components/card/Box.tsx
@@ -0,0 +1,27 @@
+import Box from "@mui/material/Box";
+
+export function DataBox({ children }) {
+  return (
+    <div style={{ width: "100%" }}>
+      <Box
+        component="span"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          p: 1,
+          m: 1,
+          bgcolor: (theme) => (theme.palette.mode === "dark" ? "#101010" : "#fff"),
+          color: (theme) => (theme.palette.mode === "dark" ? "grey.300" : "grey.800"),
+          border: "1px solid",
+          borderColor: (theme) => (theme.palette.mode === "dark" ? "grey.800" : "grey.300"),
+          borderRadius: 2,
+          fontSize: "0.875rem",
+          fontWeight: "700",
+        }}
+      >
+        {children}
+      </Box>
+    </div>
+  );
+}
--- client/src/components/card/FullClientCard.tsx
+++ client/src/components/card/FullClientCard.tsx
@@ -2,6 +2,21 @@ import { Client } from "../../data";
 import { Link } from "react-router-dom";
 import style from "./fullCard.module.css";
 import { deleteClient } from "../../api/clients";
+import Button from "@mui/material/Button";
+import { DataBox } from "./Box";
+import Avatar from "@mui/material/Avatar";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 type Props = {
   client: Client;
@@ -9,20 +24,74 @@ type Props = {
 
 export const FullClientCard: React.FC<Props> = ({ client }) => {
   return (
-    <div className={style.container}>
-      {client.zdjecie && <img src={client.zdjecie} alt={client.imie} />}
-      <p>{client.imie + " " + client.nazwisko}</p>
-      <p>{client.miasto + " " + client.kod}</p>
-      <p>{client.telefon}</p>
-      <p>zamówienia:</p>
-      <button>
-        <Link to={`/clients/${client.id}/edit`} state={client}>
-          Edit
-        </Link>
-      </button>
-      <button onClick={() => deleteClient(client.id)}>
-        <Link to={"/clients"}>DELETE</Link>
-      </button>
-    </div>
+    <DataBox className={style.box}>
+      <div className={style.container}>
+        <div className={style.left}>
+          <Avatar
+            style={{ background: "#2e3b65" }}
+            aria-label="recipe"
+            alt={client.imie + " " + client.nazwisko}
+            src={client.zdjecie}
+          >
+            R
+          </Avatar>
+        </div>
+        <div className={style.right}>
+          <p>{client.imie + " " + client.nazwisko}</p>
+          <p>{"ul. " + client.ulica}</p>
+          <p>{client.miasto + " " + client.kod}</p>
+          <p>{client.telefon}</p>
+          <p>
+            {" "}
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>Orders</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Nr zamówienia</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {client.orders &&
+                          client.orders.map((order) => (
+                            <TableRow
+                              key={order.id}
+                              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                            >
+                              <TableCell component="th" scope="row">
+                                <Link to={`/orders/${order.id}`}>{"#" + order.id}</Link>
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </p>
+          <div className={style.buttons}>
+            <Link to={`/clients/${client.id}/edit`} state={client}>
+              <Button variant="outlined">Edit</Button>
+            </Link>
+
+            <Link to={"/clients"}>
+              <Button onClick={() => deleteClient(client.id)} variant="outlined">
+                DELETE
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </DataBox>
   );
 };
