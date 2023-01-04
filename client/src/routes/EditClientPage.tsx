24b13277d236bfe7816c6a84c0b056478786a7f6
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-05 00:55:56 +0100

    added styling to edit client page

--- client/src/routes/EditClientPage.tsx
+++ client/src/routes/EditClientPage.tsx
@@ -1,32 +1,39 @@
 import { useFormik } from "formik";
 import { FormInput } from "../components/form/formInput";
-import { addClientValidationSchema } from "../utils/clientSchema";
+import { addClientValidationSchema, AddClientForm } from "../utils/clientSchema";
 import { useLocation, Link, useParams } from "react-router-dom";
 import { updateClient } from "../api/clients";
 import { ClientActionFormik } from "../utils/useFormik";
+import Button from "@mui/material/Button";
+import style from "../styles/editClient.module.css";
 
 export const EditClientPage = () => {
   const { state } = useLocation();
   const params = useParams();
 
   const formik = ClientActionFormik("update", state);
+  if (!formik) return <p>loading...</p>;
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <FormInput accessor="imie" formik={formik} />
-      <FormInput accessor="nazwisko" formik={formik} />
-      <FormInput accessor="ulica" formik={formik} />
-      <FormInput accessor="kod" formik={formik} />
-      <FormInput accessor="miasto" formik={formik} />
-      <FormInput accessor="region" formik={formik} />
-      <FormInput accessor="zdjecie" formik={formik} />
-      <FormInput accessor="telefon" formik={formik} />
-      <button type="submit">
-        <Link to={`/clients/${params.id}`}>Update</Link>
-      </button>
-      <Link to={`/clients/${params.id}`} state={state}>
-        <button>Cancel</button>
-      </Link>
+    <form onSubmit={formik.handleSubmit} className={style.form}>
+      <section className={style.inputs}>
+        {Object.keys(formik.initialValues).map(
+          (el) =>
+            el !== "orders" && <FormInput<AddClientForm> accessor={el} formik={formik} key={el} />
+        )}
+      </section>
+      <section className={style.buttons}>
+        <Link to={`/clients/${params.id}`} className={style.link}>
+          <Button onClick={formik.handleSubmit} variant="outlined" className={style.btn}>
+            Update
+          </Button>
+        </Link>
+        <Link to={`/clients/${params.id}`} className={style.link}>
+          <Button variant="outlined" className={style.btn}>
+            Cancel
+          </Button>
+        </Link>
+      </section>
     </form>
   );
 };
