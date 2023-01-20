0accf742a85bb959ddc63d67ca55edbcc8202970
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-20 20:10:35 +0100

    implemented payment logic

--- client/src/routes/InvoicesPage.tsx
+++ client/src/routes/InvoicesPage.tsx
@@ -1,14 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 
 import Button from "@mui/material/Button";
 import { useState } from "react";
 import { BasicSelect } from "../components/invoices/Select";
 import BasicTable from "../components/invoices/Table";
+import { changeOrderStatus, getOrder } from "../api/orders";
+import { deposit, withdraw } from "../store/moneySlice";
+import { pay } from "../store/orderSlice";
+import { useNotificationContext } from "../utils/NotificationContext";
 
 export const InvoicesPage = () => {
   const orders = useSelector((state: RootState) => state.order);
+  const { setSuccess, setError } = useNotificationContext();
+  const dispatch = useDispatch();
+
   const [selected, setSelected] = useState<string>("");
+
+  const handlePay = async (id: string) => {
+    try {
+      const order = await getOrder(id);
+      const payedOrder = await changeOrderStatus(order.id, "payed");
+      dispatch(deposit(payedOrder.kwota));
+      dispatch(pay(payedOrder.id));
+      setSuccess();
+    } catch {
+      setError();
+    }
+  };
 
   return (
     <>
@@ -18,7 +37,9 @@ export const InvoicesPage = () => {
         setSelected={setSelected}
         selected={selected}
       />
-      <Button variant="outlined">ROZLICZ</Button>
+      <Button variant="outlined" onClick={() => handlePay(selected)}>
+        ROZLICZ
+      </Button>
     </>
   );
 };
--- client/src/store/orderSlice.ts
+++ client/src/store/orderSlice.ts
@@ -18,33 +18,19 @@ export const moneySlice = createSlice({
   initialState,
   reducers: {
     add: (state, action: PayloadAction<string>) => {
-      // state.push({ id: "1111", tytul: "tytul" });
-      // changeOrderStatus(action.payload, "awaiting").then((res) => {
-      //   state.push({ id: res.id, tytul: res.tytul });
-      // });
       state.push({ id: action.payload.id, tytul: action.payload.tytul });
     },
     pay: (state, action: PayloadAction<string>) => {
-      //sprawdz czy takie id znajduje sie w store
-      const checkIfExists = state.some((order) => order.id === action.payload);
-      if (!checkIfExists) throw new Error("Nie ma takiego zamówenia na stanie");
-      //wyslij prosbe do servera, by zmienic status zamowienia z takim id na oplacone
-      changeOrderStatus(action.payload, "payed")
-        .then((res) => {
-          //jak sukces, wyslij kwote tego zamowienia do moneyslice
-          const dispatch = useDispatch();
-          dispatch(deposit(res.kwota));
-          //usun oplacone zamowienie z orderslice
-          state = state.filter((order) => order.id !== res.id);
-        })
-        .catch((error) => console.log(error));
+      state = state.splice(
+        state.findIndex(function (i) {
+          return i.id === action.payload;
+        }),
+        1
+      );
     },
   },
 });
 
-export const { add } = moneySlice.actions;
+export const { add, pay } = moneySlice.actions;
 
 export default moneySlice.reducer;
-
-// czy order slice ma przechowywac tylko id i tytul, czy moze cale obiekty?
-// na podstronie invoice, formularzem wybieram id zamówienia które ma zostać opłacone?
